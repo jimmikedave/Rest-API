b56baf10c7d01f1dc3f2eed73501834ea964e268
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-08-10 23:39:34 -0400

    Add authentication

--- routes/users.js
+++ routes/users.js
@@ -3,22 +3,65 @@
 const express = require('express');
 const User = require('../models').User;
 const router = express.Router();
+const bcryptjs = require('bcryptjs');
+const auth = require('basic-auth');
 
-/* Handler function to wrap each route. */
+/* Handler function to pass errors to the Global Error Handler */
 function asyncHandler(cb){
-    return async(req, res, next) => {
-      try {
-        await cb(req, res, next)
-      } catch(error){
-        res.status(500).send(error);
-      }
+  return async (req, res, next)=>{
+    try {
+      await cb(req,res, next);
+    } catch(err){
+      res.status(400).next(err);
     }
+  };
+}
+
+// Authentication Function
+const authenticateUser = (req, res, next) => {
+  let message = null;
+  const credentials = auth(req);
+
+  if(credentials) {
+    const allUsers = User.findAll();
+    const user = allUsers.find(u => u.emailAddress === credentials.emailAddress);
+
+    if(user) {
+      const authenticated = bcryptjs
+        .compareSync(credentials.pass, user.password)
+
+      if(authenticated) {
+        req.currentUser = user;
+            } else {
+              message = `Authentication failure for email: ${user.emailAddress}`;
+            }
+        } else {
+          message = `User not found for email: ${credentials.emailAddress}`;
+        }
+    } else {
+      message = 'Auth header not found';
   }
 
-// Route that returns a list of users.
-router.get('/', asyncHandler(async (req, res) => {
-    const users = await User.findAll();
-    res.json(users);
+  // If user authentication failed...
+  if(message) {
+    console.warn(message);
+
+    // Return a response with a 401 Unauthorized HTTP status code.
+    res.status(401).json({message:'Access Denied'})
+  } else {
+      // Or if user authentication succeeded...
+      // Call the next() method.
+
+      next();
+  }
+};
+
+// Route that returns the authenticated user.
+router.get('/', authenticateUser, asyncHandler(async (req, res) => {
+    const user = req.currentUser;
+
+
+    res.json(user);
 }));
 
 // Route that creates a new user
@@ -29,7 +72,7 @@ router.post('/', asyncHandler(async (req, res) => {
     //Validate that we have a value for "name"
     if(!newUser.firstName) {
       errors.push('Please provide a value for "firstName".')
-    }
+    } 
 
     if(!newUser.lastName) {
       errors.push('Please provide a value for "lastName".')
@@ -41,6 +84,8 @@ router.post('/', asyncHandler(async (req, res) => {
 
     if(!newUser.password) {
       errors.push('Please provide a value for "password".')
+    } else {
+      newUser.password = bcryptjs.hashSync(newUser.password);
     }
 
     if(errors.length > 0) {
@@ -50,6 +95,5 @@ router.post('/', asyncHandler(async (req, res) => {
       res.status(201).redirect('/');
     }
 }));
-
 
 module.exports = router;
